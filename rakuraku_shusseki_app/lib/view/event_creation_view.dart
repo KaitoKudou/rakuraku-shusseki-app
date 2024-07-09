f002151d9553807c79faff71b0f1614fc80756ce
Kaito Kudou <kaitokudou2468110921@gmail.com>
2024-07-09 23:38:20 +0900

    [fix]TextFieldをタップで日付と時間を選択可能に

--- rakuraku_shusseki_app/lib/view/event_creation_view.dart
+++ rakuraku_shusseki_app/lib/view/event_creation_view.dart
@@ -1,7 +1,50 @@
 import 'package:flutter/material.dart';
 
-class EventCreationView extends StatelessWidget {
+class EventCreationView extends StatefulWidget {
   const EventCreationView({super.key});
+
+  @override
+  State<EventCreationView> createState() => _EventCreationViewState();
+}
+
+class _EventCreationViewState extends State<EventCreationView> {
+  final TextEditingController _dateController = TextEditingController();
+  final TextEditingController _timeController = TextEditingController();
+
+  @override
+  void dispose() {
+    _dateController.dispose();
+    _timeController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null && picked != DateTime.now()) {
+      setState(() {
+        _dateController.text = "${picked.year}/${picked.month}/${picked.day}";
+      });
+    }
+  }
+
+  Future<void> _selectTime(BuildContext context) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+    );
+    if (picked != null) {
+      setState(() {
+        final formattedTime =
+            "${picked.hour.toString().padLeft(2, '0')}:${picked.minute.toString().padLeft(2, '0')}";
+        _timeController.text = formattedTime;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,6 +79,7 @@ class EventCreationView extends StatelessWidget {
               children: [
                 Expanded(
                   child: TextField(
+                    controller: _dateController,
                     decoration: InputDecoration(
                       labelText: '実施日を入力',
                       border: OutlineInputBorder(
@@ -46,6 +90,7 @@ class EventCreationView extends StatelessWidget {
                     readOnly: true,
                     onTap: () {
                       debugPrint('DatePickerを表示させる');
+                      _selectDate(context);
                     },
                     onTapOutside: (_) {
                       FocusManager.instance.primaryFocus?.unfocus();
@@ -55,6 +100,7 @@ class EventCreationView extends StatelessWidget {
                 const SizedBox(width: 16),
                 Expanded(
                   child: TextField(
+                    controller: _timeController,
                     decoration: InputDecoration(
                       labelText: '開始時刻を入力',
                       border: OutlineInputBorder(
@@ -65,6 +111,7 @@ class EventCreationView extends StatelessWidget {
                     readOnly: true,
                     onTap: () {
                       debugPrint('DateTimePickerを表示させる');
+                      _selectTime(context);
                     },
                     onTapOutside: (_) {
                       FocusManager.instance.primaryFocus?.unfocus();
