3714c4fad0d68f2f1e6876c5cc7ad29eae32b7ec
Kaito Kudou <kaitokudou2468110921@gmail.com>
2024-08-29 22:45:34 +0900

    [add]repositoryを作成

---
+++ rakuraku_shusseki_app/lib/repository/event_repository.dart
@@ -0,0 +1,55 @@
+import 'package:isar/isar.dart';
+import 'package:rakuraku_shusseki_app/model/event.dart';
+
+class EventRepository {
+  final Isar isar;
+
+  EventRepository({required this.isar});
+
+  Future<int> createEvent({
+    required String title,
+    required String date,
+    required String time,
+  }) async {
+    final Event event = Event()
+      ..eventTitle = title
+      ..effectiveDate = date
+      ..startTime = time;
+
+    await isar.writeTxn(() async {
+      await isar.events.put(event);
+    });
+
+    return event.id;
+  }
+
+  Future<void> editEvent({
+    required Event event,
+    required String title,
+    required String date,
+    required String time,
+  }) async {
+    event
+      ..eventTitle = title
+      ..effectiveDate = date
+      ..startTime = time;
+
+    await isar.writeTxn(() async {
+      await isar.events.put(event);
+    });
+  }
+
+  Future<void> deleteEvent({required Event event}) async {
+    await isar.writeTxn(() async {
+      await isar.events.delete(event.id);
+    });
+  }
+
+  Future<List<Event>> loadEventsData() async {
+    return await isar.events.where().findAll();
+  }
+
+  Future<Event?> loadEventById(int id) async {
+    return await isar.events.get(id);
+  }
+}
