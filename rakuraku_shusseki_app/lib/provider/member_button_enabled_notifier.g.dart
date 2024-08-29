9d80cd5f1e1d89dba7c8f7de239e2257ca48030d
Kaito Kudou <kaitokudou2468110921@gmail.com>
2024-08-30 00:05:57 +0900

    [fix]元あったクラス名をリネーム

---
+++ rakuraku_shusseki_app/lib/provider/member_button_enabled_notifier.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'member_button_enabled_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$memberButtonEnabledNotifierHash() =>
+    r'c52eb08fa933120750fc0cf4fac274fae449d521';
+
+/// See also [MemberButtonEnabledNotifier].
+@ProviderFor(MemberButtonEnabledNotifier)
+final memberButtonEnabledNotifierProvider =
+    AutoDisposeNotifierProvider<MemberButtonEnabledNotifier, bool>.internal(
+  MemberButtonEnabledNotifier.new,
+  name: r'memberButtonEnabledNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$memberButtonEnabledNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$MemberButtonEnabledNotifier = AutoDisposeNotifier<bool>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
