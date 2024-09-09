9668c70de29f84848628dcd7e44de522662b275d
Kaito Kudou <kaitokudou2468110921@gmail.com>
2024-09-09 22:40:09 +0900

    [fix]名前を変更

--- rakuraku_shusseki_app/lib/provider/event_list_state_notifier.g.dart
+++ rakuraku_shusseki_app/lib/provider/event_list_state_notifier.g.dart
@@ -1,27 +1,27 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'event_state_notifier.dart';
+part of 'event_list_state_notifier.dart';
 
 // **************************************************************************
 // RiverpodGenerator
 // **************************************************************************
 
-String _$eventStateNotifierHash() =>
-    r'cd27b1c3d9fd6ae9bee5dffa68fe3a9275796e8e';
+String _$eventListStateNotifierHash() =>
+    r'ac8ba946c564ec2598f025fac1ae0278350c38b0';
 
-/// See also [EventStateNotifier].
-@ProviderFor(EventStateNotifier)
-final eventStateNotifierProvider =
-    AutoDisposeNotifierProvider<EventStateNotifier, List<Event>>.internal(
-  EventStateNotifier.new,
-  name: r'eventStateNotifierProvider',
+/// See also [EventListStateNotifier].
+@ProviderFor(EventListStateNotifier)
+final eventListStateNotifierProvider =
+    AutoDisposeNotifierProvider<EventListStateNotifier, List<Event>>.internal(
+  EventListStateNotifier.new,
+  name: r'eventListStateNotifierProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
       ? null
-      : _$eventStateNotifierHash,
+      : _$eventListStateNotifierHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
-typedef _$EventStateNotifier = AutoDisposeNotifier<List<Event>>;
+typedef _$EventListStateNotifier = AutoDisposeNotifier<List<Event>>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
